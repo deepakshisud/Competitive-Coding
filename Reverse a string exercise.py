dad5c17de79a9c0774476a8c09dcda86d439510a
Deepakshi <deepakshisud@gmail.com>
2020-12-08 10:49:57 +0530

    Arrays

---
+++ Reverse a string exercise.py
@@ -0,0 +1,8 @@
+# Create a function that reverses a string
+#'Hi my name is Deepakshi' should be:
+#'ihskapeeD si eman ym iH'
+
+def reverse(str):
+    return str[::-1]
+
+print(reverse('Hi my name is Deepakshi'))
