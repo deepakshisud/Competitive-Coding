0e07f66a016159f9a2b41344a89045f992267c0e
Deepakshi <deepakshisud@gmail.com>
2021-01-28 19:38:53 +0530

    Bubble sort added

--- Cpp implementations/Bubble_sort.cpp
+++ Cpp implementations/Bubble_sort.cpp
@@ -3,9 +3,18 @@ using namespace std;
 
 void bubbleSort(int a[], int n) {
 	for(int i=1;i<=n-1;i++) {
+		
+		int flag = 0;
+		
 		for(int j=0;j<=n-2;j++) {
-			if(a[j]>a[j+1])
+			if(a[j]>a[j+1]) {
 				swap(a[j],a[j+1]);
+				flag=1;
+			}
+		}
+		
+		if(flag==0) {   ///array already sorted
+			break;
 		}
 	}
 }
