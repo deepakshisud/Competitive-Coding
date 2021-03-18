aa6d90a570c2d2eb69b58135f467c26f0c2129cf
Deepakshi <deepakshisud@gmail.com>
2021-03-18 22:15:20 +0530

    Counting leaf nodes added

--- Cpp implementations/Trees/CountLeafNodes.cpp
+++ Cpp implementations/Trees/CountLeafNodes.cpp
@@ -37,45 +37,20 @@ TreeNode<int>* takeInput() {
 	return root;
 }
 
-int countNodes(TreeNode<int>* root) {
-	if(root==NULL) return 0;
-	int count = 1;
+int countLeafNodes(TreeNode<int>* root) {
+	if(root->children.size()==0) {
+		return 1;
+	}
+	int ans = 0;
 	for(int i=0; i<root->children.size(); i++) {
-		count += countNodes(root->children[i]);
+		ans += countLeafNodes(root->children[i]);
 	}
-	return count;
+	return ans;
 }
-
-int treeHeight(TreeNode<int>* root) {
-	int h = 0;
-	for(int i = 0; i<root->children.size(); i++) {
-		int smallAns = treeHeight(root->children[i]);
-		h = max(h,smallAns);
-	}
-	h++;
-	return h;
-}
-
-void printKlevelNodes(TreeNode<int>* root, int k) {
-	if(k==0) {
-		cout<<root->data<<" ";
-		return;
-	} else {
-		for(int i=0; i<root->children.size(); i++) {
-			printKlevelNodes(root->children[i], k-1);			
-		}
-	}
-}
-
 
 int main() {
 	TreeNode<int>* root = takeInput();
-	cout<<"The number of nodes: "<<countNodes(root)<<endl;
-	cout<<"The tree height: "<<treeHeight(root)<<endl;
-	int k;
-	cout<<"Enter the kth level: ";
-	cin>>k;
-	cout<<"The nodes at "<<k<<" are: ";
-	printKlevelNodes(root,k);
+	cout<<"No of leaf nodes: "<<countLeafNodes(root);
+	
 	return 0;
 }
