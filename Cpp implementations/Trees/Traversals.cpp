1a50f20fb680cad9439b71334adf3645a266b5c1
Deepakshi <deepakshisud@gmail.com>
2021-03-19 20:11:33 +0530

    Binary Trees started

--- Cpp implementations/Trees/Traversals.cpp
+++ Cpp implementations/Trees/Traversals.cpp
@@ -10,6 +10,12 @@ class TreeNode {
 		TreeNode(T data) {
 			this->data = data;
 		}
+		
+		~TreeNode() {
+	    	for(int i=0;i<children.size();i++) {
+	    		delete children[i];
+	    	}
+		}
 };
 
 TreeNode<int>* takeInput() {
