4a17284bb8b6e8c007816158e0f3185786ded5be
Deepakshi <deepakshisud@gmail.com>
2021-03-19 21:51:51 +0530

    Binary Trees Level-wise input done

--- Cpp implementations/Trees/Binary Trees/takeInputLevel.cpp
+++ Cpp implementations/Trees/Binary Trees/takeInputLevel.cpp
@@ -20,17 +20,39 @@ class BTNode{
 		}
 };
 
-BTNode<int>* takeInput() {
+BTNode<int>* takeInputLevel() {
 	int rootData;
-	cout<<"Enter data: "<<endl;
+	cout<<"Enter root data: ";
 	cin>>rootData;
-	if(rootData== -1) return NULL;
 	BTNode<int>* root = new BTNode<int>(rootData);
-	root->left = takeInput();
-	root->right = takeInput();
-	
+	queue<BTNode<int>*> q;
+	q.push(root);
+	while(q.size()) {
+		BTNode<int>* f = q.front();
+		q.pop();
+		int leftData;
+		cout<<"Enter the left child of "<<f->data<<" : ";
+		cin>>leftData;
+		if(leftData == -1) f->left = NULL; 
+		else {
+			BTNode<int>* leftNode = new BTNode<int>(leftData);
+			q.push(leftNode);
+			f->left = leftNode;
+		}
+		int rightData;
+		cout<<"Enter the right child of "<<f->data<<" : ";
+		cin>>rightData;
+		if(rightData == -1) f->right = NULL; 
+		else {
+			BTNode<int>* rightNode = new BTNode<int>(rightData);
+			q.push(rightNode);
+			f->right = rightNode;
+		}
+	}
 	return root;
 }
+
+
 
 void printTree(BTNode<int>* root) {
 	if(root==NULL) return;
@@ -46,7 +68,7 @@ void printTree(BTNode<int>* root) {
 
 int main() {
 	
-	BTNode<int>* root = takeInput();
+	BTNode<int>* root = takeInputLevel();
 	
 	printTree(root);
 	
